59602db2fa20d5b9c090be11cfe9596a36307802
H3rd7 <vagner_h3rd7@outlook.com>
2019-12-15 14:43:05 -0300

    atualizando ViewModel e vModelErros

--- gerAcademic/Models/ViewModels/ErrorViewModel.cs
+++ gerAcademic/Models/ViewModels/ErrorViewModel.cs
@@ -1,10 +1,12 @@
 using System;
 
-namespace gerAcademic.Models
+namespace gerAcademic.Models.ViewModels
 {
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        
+        public string Message { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
