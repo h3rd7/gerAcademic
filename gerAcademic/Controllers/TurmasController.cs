ef2bb377f3d1689e4d1a3d3114992b0498a3686e
H3rd7 <vagner_h3rd7@outlook.com>
2019-12-15 02:38:56 -0300

    add Scaffolding de Turma

---
+++ gerAcademic/Controllers/TurmasController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using gerAcademic.Data;
+using gerAcademic.Models;
+
+namespace gerAcademic.Controllers
+{
+    public class TurmasController : Controller
+    {
+        private readonly DataContext _context;
+
+        public TurmasController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Turma.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var turma = await _context.Turma
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (turma == null)
+            {
+                return NotFound();
+            }
+
+            return View(turma);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome")] Turma turma)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(turma);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(turma);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var turma = await _context.Turma.FindAsync(id);
+            if (turma == null)
+            {
+                return NotFound();
+            }
+            return View(turma);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome")] Turma turma)
+        {
+            if (id != turma.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(turma);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TurmaExists(turma.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(turma);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var turma = await _context.Turma
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (turma == null)
+            {
+                return NotFound();
+            }
+
+            return View(turma);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var turma = await _context.Turma.FindAsync(id);
+            _context.Turma.Remove(turma);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TurmaExists(int id)
+        {
+            return _context.Turma.Any(e => e.Id == id);
+        }
+    }
+}
